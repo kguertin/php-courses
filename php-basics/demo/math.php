931c7b0bdd4c47f4fe2e9b80d672247af27c1bf7
kguertin <kevin.m.guertin@gmail.com>
2020-07-04 17:25:16 -0400

    basic php math operations

---
+++ php-basics/demo/math.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo 56 + 45;
+        echo "<br>";
+        echo 30 - 20;
+        echo "<br>";
+        echo 10 * 10;
+        echo "<br>";
+        echo 10 / 2;
+        echo "<br>";
+        echo "<br>";
+        echo 4 + 5 * 5;
+        echo "<br>";
+        $one = 1;
+        $two = 2;
+        echo $one + $two;
+
+    ?>
+</body>
+</html>
