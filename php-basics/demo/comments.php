b56eedffc159599de7f9f573eef6094d8fe484ff
kguertin <kevin.m.guertin@gmail.com>
2020-07-04 16:20:07 -0400

    MAMP setup and comments variables in php

---
+++ php-basics/demo/comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // How to write comments (single line)
+
+        /*
+        This is a multi-line comment
+        */
+
+        // This function below displays text
+        
+        echo "Hello World"
+    ?>
+</body>
+</html>
