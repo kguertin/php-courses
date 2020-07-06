df83c31ec2d887d7a9bed3fb50dbc4ceaf22a6d2
kguertin <kevin.m.guertin@gmail.com>
2020-07-05 21:40:50 -0400

    foreach loop php

---
+++ php-basics/demo/for_each_loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $numbers =  array(1, 2, 3, 4, 5);
+
+        foreach($numbers as $number){
+            echo $number . "<br>";
+        }
+    ?>
+</body>
+</html>
