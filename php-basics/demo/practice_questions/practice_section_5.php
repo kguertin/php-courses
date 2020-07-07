23a94703dbb1d0ddd8f77fa7c76807d1b9d603fb
kguertin <kevin.m.guertin@gmail.com>
2020-07-06 20:04:15 -0400

    practice with prebuilt php functions

---
+++ php-basics/demo/practice_questions/practice_section_5.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice Section 5</title>
+</head>
+<body>
+    <?php
+
+        echo ceil(1.345623542342346678783);
+        echo "<br>";
+        $string = "Hello World!";
+        echo crypt($string, 10); // second value is number of salts like node crypto
+        echo "<br>";
+        $list = ['never', 'eat', 'shredded', 'wheat'];
+        print_r($list);
+        echo "<br>";
+        array_pop($list);
+        print_r($list);
+        echo "<br>";
+        array_push($list, 'feets');
+        print_r($list);
+
+    ?>
+</body>
+</html>
