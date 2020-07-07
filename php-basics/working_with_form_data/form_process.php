c26e723533ba72a63c462ee9b10481c6a31f6d48
kguertin <kevin.m.guertin@gmail.com>
2020-07-07 18:13:58 -0400

    php send form data to another page via form action

--- php-basics/working_with_form_data/form_process.php
+++ php-basics/working_with_form_data/form_process.php
@@ -1,4 +1,4 @@
-<!-- Single page validation  -->
+
 <?php
 // super global variable is associative array
 
@@ -28,19 +28,3 @@ if(isset($_POST["submit"])) {
     echo "username: ". $username . " Password: " . $password;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Working With Form Data</title>
-</head>
-<body>
-    <form action="form.php" method="POST">
-        <input type="text" name="username" placeholder="Enter Username">
-        <input type="password" name="password" placeholder="Enter Password"><br>
-        <input type="submit" name="submit">
-    </form>
-</body>
-</html>
