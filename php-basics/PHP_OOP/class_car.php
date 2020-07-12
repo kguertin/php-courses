b6945340f47662c15ac2aad03f08284b9b35d3e4
kguertin <kevin.m.guertin@gmail.com>
2020-07-11 20:45:30 -0400

    add properties and make objects

--- php-basics/PHP_OOP/class_car.php
+++ php-basics/PHP_OOP/class_car.php
@@ -1,15 +1,33 @@
 <?PHP
 
 class Car {
+
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 1;
+
     function moveWheels(){
-        echo "Wheels Move";
-    }    
+        $this->wheels = 10;
+    }  
+
 }
 
-if(method_exists("Car", "moveWheels")){
-    echo "the method exists";
-}
+$bmw = new Car();
+ // this is like dot notation;
+echo "<br>";
 
+echo "Wheels: " . $bmw->wheels;
 
+$bmw->wheels = 8;
+echo "<br>";
+echo "Wheels: " . $bmw->wheels;
 
-?>
+$bmw->moveWheels();
+echo "<br>";
+echo "Wheels: " . $bmw->wheels;
+
+$truck = new Car();
+echo "<br>";
+echo "Truck Wheels: " . $truck->wheels = 18;
+
