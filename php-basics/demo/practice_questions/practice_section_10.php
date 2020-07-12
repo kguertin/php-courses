d161379475efde8017615714cb030d38d8b429ea
kguertin <kevin.m.guertin@gmail.com>
2020-07-12 13:40:21 -0400

    php practice using classes and objects

---
+++ php-basics/demo/practice_questions/practice_section_10.php
@@ -0,0 +1,27 @@
+<?php
+
+class Dog {
+    public $fur = "black";
+    public $snoot = "booped";
+    public $pupper = "false" ;
+
+    function ShowAll(){
+        echo "Fur: " . $this->fur;
+        echo "<br>";
+        echo "Snoot status: " . $this->snoot;
+        echo "<br>";
+        echo "A Pupper? " . $this->pupper;
+
+    }
+}
+
+$doggo = new Dog();
+$doggo->ShowAll();
+echo "<br>";
+
+class Pupper extends Dog {
+    public $pupper = "true";
+}
+
+$pupper = new Pupper();
+$pupper->ShowAll();
