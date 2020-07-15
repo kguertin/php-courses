91e257bbe85facdc80c3c049ce465a49f8eb6e1e
kguertin <kevin.m.guertin@gmail.com>
2020-07-15 17:21:39 -0400

    can submit new post data via admin page; ;

--- php-basics/cms_project/admin/includes/add_post.php
+++ php-basics/cms_project/admin/includes/add_post.php
@@ -15,6 +15,13 @@
 
         move_uploaded_file($post_image_temp, "../images/$post_image"); // Where we save the file
 
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) "; 
+
+        $query .= "VALUES ({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', {$post_comment_count}, '{$post_status}' ) ";
+
+        $add_post = mysqli_query($connection, $query);
+
+        confirm_query($add_post);
     }
 
 ?>
