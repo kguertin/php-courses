471888ebc7673dba1593ddb4e39d9c2ffd8c27d1
kguertin <kevin.m.guertin@gmail.com>
2020-07-15 13:40:17 -0400

    set up html for posts page

--- php-basics/cms_project/admin/includes/navigation.php
+++ php-basics/cms_project/admin/includes/navigation.php
@@ -38,7 +38,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#post_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="post_dropdown" class="collapse">
                             <li>
-                                <a href="#">View Posts</a>
+                                <a href="./posts.php">View Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
@@ -46,7 +46,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                        <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
                     <li class="active">
                         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
