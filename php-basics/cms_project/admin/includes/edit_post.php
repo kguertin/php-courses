2cc616701185ec764f4484ecae9e4a53812238fc
kguertin <kevin.m.guertin@gmail.com>
2020-07-15 22:27:39 -0400

    able to update post data, and fixed image issue

--- php-basics/cms_project/admin/includes/edit_post.php
+++ php-basics/cms_project/admin/includes/edit_post.php
@@ -2,7 +2,7 @@
     if(isset($_GET['pid'])){
         $post_id_edit = $_GET['pid'];
     }
-    $query = "SELECT * FROM posts";
+    $query = "SELECT * FROM posts WHERE post_id = {$post_id_edit}";
     $select_posts = mysqli_query($connection, $query);
     
     while($row = mysqli_fetch_assoc($select_posts)){
@@ -18,6 +18,48 @@
         $post_comment_count = $row["post_comment_count"];
 
     }
+
+    if(isset($_POST['update_post'])){
+
+        $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST["post_category"];
+        $post_status = $_POST["post_status"];
+        $post_image = $_FILES['post_image']['name'];
+        $post_image_temp = $_FILES['post_image']['tmp_name'];
+        $post_content = $_POST["post_content"];
+        $post_tags = $_POST["post_tags"];
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        if(empty($post_image)) {
+            $query = "SELECT post_image FROM posts WHERE post_id = $post_id_edit ";
+            $select_image = mysqli_query($connection, $query);
+            confirm_query($select_image);
+
+            $result = mysqli_fetch_assoc($select_image);
+            $post_image = $result["post_image"];
+
+            
+        }
+
+        $query = "UPDATE posts SET ";
+        $query .= "post_title = '{$post_title}', ";
+        $query .= "post_category_id = '{$post_category_id}', ";
+        $query .= "post_date = now(), ";
+        $query .= "post_author = '{$post_author}', ";
+        $query .= "post_status = '{$post_status}', ";
+        $query .= "post_tags = '{$post_tags}', ";
+        $query .= "post_content = '{$post_content}', ";
+        $query .= "post_image = '{$post_image}' ";
+        $query .= "WHERE post_id = {$post_id_edit} ";
+
+        $update_post = mysqli_query($connection, $query);
+        // header("Location: posts.php");
+        
+        confirm_query($update_post);
+
+    }
 ?>
 
 
@@ -28,7 +70,7 @@
     </div>
 
     <div class="form-group">
-        <select name="" id="">
+        <select name="post_category" id="">
             <?php
                 $query = "SELECT * FROM categories";
                 $get_edit_categories = mysqli_query($connection, $query);
@@ -58,6 +100,7 @@
 
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="Post Image">
+        <input type="file" class="form-control" name="post_image" id="post_image">
     </div>
 
     <div class="form-group">
@@ -71,7 +114,7 @@
     </div>
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+        <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
     </div>
 
 
