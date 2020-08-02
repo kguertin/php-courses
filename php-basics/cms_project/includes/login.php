7f3f6095f5b2c9a4d381e813c173f3fd63a17677
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-02 19:41:36 -0400

    set up sessions and echo data

--- php-basics/cms_project/includes/login.php
+++ php-basics/cms_project/includes/login.php
@@ -1,4 +1,5 @@
-<?php include "db.php" ?>
+<?php include "db.php" ?>\
+<?php session_start(); ?>
 
 <?php
 if(isset($_POST['login'])) {
@@ -17,10 +18,11 @@ if(isset($_POST['login'])) {
     }
 
     $query_user_table = mysqli_fetch_array($select_user_query);
+
     $db_id = $query_user_table['user_id'];
     $db_first_name = $query_user_table['user_first_name'];
     $db_last_name = $query_user_table['user_last_name'];
-    $db_role = $query_user_table['user_role'];
+    $db_User_role = $query_user_table['user_role'];
     $db_username = $query_user_table['username'];
     $db_password = $query_user_table['user_password'];
 
@@ -28,6 +30,11 @@ if(isset($_POST['login'])) {
     if($username !== $db_username && $password !== $db_password){
         header("Location: ../index.php");
     } else if ($username == $db_username && $password == $db_password) {
+        $_SESSION['username'] = $db_username;
+        $_SESSION['first_name'] = $db_first_name;
+        $_SESSION['last_name'] = $db_last_name;
+        $_SESSION['user_role'] = $db_user_role;
+
         header("Location: ../admin"); 
     } else {
         header("Location: ../index.php");
