6ec8cb8270d78f0255f19a3e7223137e03ba6fae
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-02 21:13:19 -0400

    refactor login form

--- php-basics/cms_project/includes/login.php
+++ php-basics/cms_project/includes/login.php
@@ -27,9 +27,7 @@ if(isset($_POST['login'])) {
     $db_password = $query_user_table['user_password'];
 
 
-    if($username !== $db_username && $password !== $db_password){
-        header("Location: ../index.php");
-    } else if ($username == $db_username && $password == $db_password) {
+    if($username === $db_username && $password === $db_password){
         $_SESSION['username'] = $db_username;
         $_SESSION['first_name'] = $db_first_name;
         $_SESSION['last_name'] = $db_last_name;
