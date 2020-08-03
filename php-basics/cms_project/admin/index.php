260a95bbcddd195ecb3b27016213de9be6f28b04
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-03 19:14:04 -0400

    add google chart to admin panel

--- php-basics/cms_project/admin/index.php
+++ php-basics/cms_project/admin/index.php
@@ -133,6 +133,37 @@
                 </div>
                                 <!-- /.row -->
 
+                <div class="row">
+                    <script type="text/javascript">
+                    google.charts.load('current', {'packages':['bar']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        const data = google.visualization.arrayToDataTable([
+                        ['Year', 'Sales', 'Expenses', 'Profit'],
+                        ['2014', 1000, 400, 200],
+                        ['2015', 1170, 460, 250],
+                        ['2016', 660, 1120, 300],
+                        ['2017', 1030, 540, 350]
+                        ]);
+
+                        const options = {
+                        chart: {
+                            title: 'Company Performance',
+                            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                        }
+                        };
+
+                        const chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                    }
+                    </script>
+                    <div id="columnchart_material" style="width: 900px; height: 500px;"></div>
+
+
+                </div>
+
             </div>
             <!-- /.container-fluid -->
 
