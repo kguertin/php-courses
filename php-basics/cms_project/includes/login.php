e45a3cb2d73e9719c553a1e57a16f9fabf282db8
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-02 20:07:15 -0400

    only admins can access admin dashboard using sessions

--- php-basics/cms_project/includes/login.php
+++ php-basics/cms_project/includes/login.php
@@ -22,7 +22,7 @@ if(isset($_POST['login'])) {
     $db_id = $query_user_table['user_id'];
     $db_first_name = $query_user_table['user_first_name'];
     $db_last_name = $query_user_table['user_last_name'];
-    $db_User_role = $query_user_table['user_role'];
+    $db_user_role = $query_user_table['user_role'];
     $db_username = $query_user_table['username'];
     $db_password = $query_user_table['user_password'];
 
