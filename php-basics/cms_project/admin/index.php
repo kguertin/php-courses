347c2804bc73d256e00e57f59d274eb375b69e64
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-03 23:02:57 -0400

    work on chart display

--- php-basics/cms_project/admin/index.php
+++ php-basics/cms_project/admin/index.php
@@ -140,17 +140,14 @@
 
                     function drawChart() {
                         const data = google.visualization.arrayToDataTable([
-                        ['Year', 'Sales', 'Expenses', 'Profit'],
-                        ['2014', 1000, 400, 200],
-                        ['2015', 1170, 460, 250],
-                        ['2016', 660, 1120, 300],
-                        ['2017', 1030, 540, 350]
+                        ['Data', 'Count'],
+                        ['Posts', 1000],
                         ]);
 
                         const options = {
                         chart: {
-                            title: 'Company Performance',
-                            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                            title: '',
+                            subtitle: '',
                         }
                         };
 
@@ -159,7 +156,7 @@
                         chart.draw(data, google.charts.Bar.convertOptions(options));
                     }
                     </script>
-                    <div id="columnchart_material" style="width: 900px; height: 500px;"></div>
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
 
 
                 </div>
