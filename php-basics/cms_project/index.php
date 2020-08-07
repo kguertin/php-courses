f049b7290906c7c9bd3226642b80c2d33fc0cfed
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-07 17:00:45 -0400

    limit five posts per page and have functioning pagination

--- php-basics/cms_project/index.php
+++ php-basics/cms_project/index.php
@@ -11,13 +11,25 @@
             <div class="col-md-8">
             <?php
 
+                if(isset($_GET['page'])){
+                    $page = $_GET['page'];
+                } else {
+                    $page = '';
+                }
+
+                if($page == '' || $page == 1){
+                    $page_1 = 0;
+                } else {
+                    $page_1 = ($page * 5) - 5;
+                }
+
                 $post_query_count = "SELECT * FROM posts";
-                $find_count = mysqli_query($connection, $query);
+                $find_count = mysqli_query($connection, $post_query_count);
                 $count = mysqli_num_rows($find_count);
                 $count = ceil($count / 5);
 
 
-                $query = "SELECT * FROM posts WHERE post_status = 'published' ";
+                $query = "SELECT * FROM posts WHERE post_status = 'published' LIMIT $page_1, 5";
                 $select_published_posts = mysqli_query($connection, $query);
                 
                 if(mysqli_num_rows($select_published_posts) === 0){
