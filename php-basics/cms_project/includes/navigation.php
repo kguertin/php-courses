95197d725fa0ee3f361ce6f83a907370111c5597
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-07 23:24:03 -0400

    can count online users

--- php-basics/cms_project/includes/navigation.php
+++ php-basics/cms_project/includes/navigation.php
@@ -1,4 +1,3 @@
-<?php include "db.php"; ?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
