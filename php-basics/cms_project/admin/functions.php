0cf0853f5bf0bf7fc0ce21ba09726b509c89f80d
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-08 17:53:33 -0400

    display number of users that have been active over period of time

--- php-basics/cms_project/admin/functions.php
+++ php-basics/cms_project/admin/functions.php
@@ -8,6 +8,29 @@ function confirm_query($result) {
     }
 }
 
+function users_online() {
+    global $connection;
+
+    $session = session_id();
+    $time = time();
+    $time_out_in_sec = 60;
+    $time_out = $time - $time_out_in_sec;
+
+    $query = "SELECT * FROM users_online WHERE session = '{$session}'";
+    $send_query = mysqli_query($connection, $query);
+    confirm_query($send_query);
+    $count = mysqli_num_rows($send_query);
+
+    if ($count == NULL) {
+        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('{$session}', '{$time}')");
+    } else {
+        mysqli_query($connection, "UPDATE users_online SET time = '{$time}' WHERE session = '{$session}'");
+    }
+
+    $users_online = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+    return $user_count = mysqli_num_rows($users_online);
+}
+
 function insert_categories(){
     global $connection;
 
