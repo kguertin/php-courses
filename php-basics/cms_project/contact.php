0706e4553572cbdb083650b5c6d3a1f8e1e48295
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-12 14:19:38 -0400

    use send mail function

--- php-basics/cms_project/contact.php
+++ php-basics/cms_project/contact.php
@@ -6,35 +6,15 @@
 
     <?php
         if(isset($_POST['submit'])){
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
+            $to = "kevin.m.guertin@gmail.com";
+            $subject = wordwrap($_POST['subject'], 70);
+            $body = $_POST['body'];
 
-            if(!empty($username) && !empty($email) && !empty($password)){
+            $header = 'From: ' . $_POST['email'];
 
-                $username = mysqli_real_escape_string($connection, $username);
-                $email = mysqli_real_escape_string($connection, $email);
-                $password = mysqli_real_escape_string($connection, $password);
-
-                $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
-    
-                $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
-                $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber')";
-                $register_user_query = mysqli_query($connection, $query);
-                
-                if(!$register_user_query){
-                    die('Query Failed' . mysqli_error($connection) . mysqli_errno($connection));
-                }
-
-                $message = "Registration Successful";
-            } else {
-                $message = "Fields cannot be empty";
-            }
-
-           
-        } else {
-            $message = '';
+            mail($to, $subject, $body, $header);
         }
+
     ?>
     
  
@@ -47,8 +27,7 @@
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
                 <h1>Contact</h1>
-                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                        <h6 class="text-center"><?php echo $message ?></h6>
+                    <form role="form" action="" method="post" id="contact-form" autocomplete="off">
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
                             <input type="email" name="email" id="email" class="form-control" placeholder="Enter Your Email">
