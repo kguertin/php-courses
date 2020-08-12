787c08cfaf92a94bca577da1c779f9df39f028b5
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-12 12:00:07 -0400

    contact page html

--- php-basics/cms_project/contact.php
+++ php-basics/cms_project/contact.php
@@ -46,23 +46,23 @@
         <div class="row">
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
-                <h1>Register</h1>
+                <h1>Contact</h1>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                         <h6 class="text-center"><?php echo $message ?></h6>
-                        <div class="form-group">
-                            <label for="username" class="sr-only">username</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
-                        </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter Your Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="subject" class="sr-only">Subject</label>
+                            <input type="text" name="subject" id="subject" class="form-control" placeholder="">
                         </div>
                          <div class="form-group">
-                            <label for="password" class="sr-only">Password</label>
-                            <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                            <label for="body" class="sr-only">Body</label>
+                            <textarea name="body" id="body" class="form-control"></textarea>
                         </div>
                 
-                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Submit">
                     </form>
                  
                 </div>
