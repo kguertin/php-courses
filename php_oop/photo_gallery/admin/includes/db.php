271d5449d0e1acb523c4ee66bef983fc4be71930
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-12 22:37:28 -0400

    add query function to db class

--- php_oop/photo_gallery/admin/includes/db.php
+++ php_oop/photo_gallery/admin/includes/db.php
@@ -16,6 +16,14 @@ class Database {
             die('Database connection failed. ' . mysqli_error());
         }
     }
+
+    public function query($sql){
+        $result = mysqli_query($this->connection, $sql);
+        if(!$result){
+            die('Query Failed');
+        }
+        return $result
+    }
 }
 
 $db = new Database;
