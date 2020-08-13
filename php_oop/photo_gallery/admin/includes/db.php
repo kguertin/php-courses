3f2d9ee379586eabd5ea873157cca6d37ad52351
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-12 22:27:36 -0400

    add db class and config file

---
+++ php_oop/photo_gallery/admin/includes/db.php
@@ -0,0 +1,18 @@
+<?php
+require_once('db_config.php');
+
+class Database {
+
+    private $connection;
+
+    public function db_connect() {
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
+        if(mysqli_connect_errno()){
+            die('Database connection failed. ' . mysqli_error());
+        }
+    }
+}
+
+$db = new Database();
+$db->db_connect();
