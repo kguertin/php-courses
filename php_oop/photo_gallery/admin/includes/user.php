e078a254aad2a21699f9331006f4eee1b906b142
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-13 20:22:02 -0400

    add method that handles query submission

--- php_oop/photo_gallery/admin/includes/user.php
+++ php_oop/photo_gallery/admin/includes/user.php
@@ -3,16 +3,20 @@
 class User {
 
 
-    public static function find_all_users(){
-        global $db;
-        $get_users = $db->query("SELECT * FROM users");
-        return $get_users;
+    public static function find_all_users(){   
+        return self::submit_query("SELECT * FROM users");
     }
 
     public static function find_user_by_id($id){
         global $db;
-        $get_user = $db->query("SELECT * FROM users WHERE user_id = {$id}");
+        $get_user = self::submit_query("SELECT * FROM users WHERE user_id = {$id} LIMIT 1");
         $found_user = mysqli_fetch_array($get_user);
         return $found_user;
+    }
+
+    public static function submit_query($sql){
+        global $db;
+        $result = $db->query($sql);
+        return $result;
     }
 }
