253243c33377efb4ba9f2bae8018c6be75a9b6d1
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-25 16:12:21 -0400

    check uploaded file structure

---
+++ php_oop/upload.php
@@ -0,0 +1,20 @@
+<?php
+echo "<pre>";
+print_r($_FILES['file_upload']);
+echo "<pre>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file_upload"><br />
+    <input type="submit" name="submit">
+    </form>
+</body>
+</html>
