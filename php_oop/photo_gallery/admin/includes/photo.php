688d58d3494d46a4007a81d667d4380ef81106b9
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-28 14:45:33 -0400

    create properties for photo model

--- php_oop/photo_gallery/admin/includes/photo.php
+++ php_oop/photo_gallery/admin/includes/photo.php
@@ -1,5 +1,13 @@
 <?php
 
 class Photo extends Db_object {
+    protected static $db_table = "photos";
+    protected static $db_table_fields = array('photo_title', 'photo_description', "photo_file_name", "photo_type", "photo_size");
+    public $photo_id;
+    public $photo_title;
+    public $photo_description;
+    public $photo_file_name;
+    public $photo_type;
+    public $photo_size;
 
 }
