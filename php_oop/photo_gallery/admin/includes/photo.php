f6ccffce525a75827cd5ea01ed988de8138cf1b9
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-28 19:35:21 -0400

    build method to set file info

--- php_oop/photo_gallery/admin/includes/photo.php
+++ php_oop/photo_gallery/admin/includes/photo.php
@@ -1,6 +1,7 @@
 <?php
 
-class Photo extends Db_object {
+class Photo extends Db_object
+{
     protected static $db_table = "photos";
     protected static $db_table_fields = array('title', 'description', "file_name", "type", "size");
     public $id;
@@ -13,7 +14,7 @@ class Photo extends Db_object {
     public $tmp_path;
     public $upload_directory = "images";
 
-    public $custom_err = array();
+    public $errors = array();
     public $upload_error_arr = array(
         UPLOAD_ERR_OK => "There is no error.",
         UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive.",
@@ -22,8 +23,22 @@ class Photo extends Db_object {
         UPLOAD_ERR_NO_FILE => "No file was uploaded.",
         UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder",
         UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk.",
-        UPLOAD_ERR_EXTENSION => "A PHP extension  stopped the file upload."
+        UPLOAD_ERR_EXTENSION => "A PHP extension  stopped the file upload.",
     );
 
-    
+    public function set_file($file){
+
+        if(empty($file) || !$file || !is_array($file)){
+            $this->errors[] = "There was no file uploaded.";
+            return false;
+        } else if($file['error'] != 0 ){
+            $this->errors[] = $this->upload_error_arr[$file['error']];
+            return false;
+        } else {
+            $this->file_name = basename($file['name']);
+            $this->tmp_path = $file['tmp_name'];
+            $this->type = $file['type'];
+            $this->size = $file['size'];
+        }
+    }
 }
