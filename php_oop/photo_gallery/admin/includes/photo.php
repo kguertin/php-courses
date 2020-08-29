3eea743204c5e5e013eca6556284707451f57468
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-28 20:11:20 -0400

    add more error handling to photo model

--- php_oop/photo_gallery/admin/includes/photo.php
+++ php_oop/photo_gallery/admin/includes/photo.php
@@ -57,7 +57,20 @@ class Photo extends Db_object
 
             $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->file_name;
 
-            $this->create();
+            if(file_exists($target_path)){
+                $this->errors[] = "The file {$this->file_name} already exists";
+                return false;
+            }
+
+            if(move_uploaded_file($this->tmp_path, $target_path)){
+                if($this->create()){
+                    unset($this->tmp_path);
+                    return true;
+                }
+            } else {
+                $this->errors[] = "The file directory may not have the right permissions";
+                return false;
+            }
         }
     }
 }
