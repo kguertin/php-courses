1d8d3ef9fada8f0a546d98e4b5bb8ff69d4652a5
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-16 14:49:19 -0400

    create delete photo php

--- php_oop/photo_gallery/admin/delete_photo.php
+++ php_oop/photo_gallery/admin/delete_photo.php
@@ -1,4 +1,15 @@
 <?php include("includes/init.php"); ?>
 <?php if(!$session->is_signed_in()) {redirect('login.php'); } ?>
 
-<?php echo 'IT WORKS' ?>
+<?php 
+if(empty($_GET['id'])){
+    redirect('photos.php');
+}
+
+$photo = Photo::find_by_id($_GET['id']);
+
+if($photo) {
+    $photo->delete_photo();
+} else {
+    redirect('photos.php');
+}
