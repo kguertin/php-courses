e685907a68ac12385812cb53e26ad7049904eedc
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-16 22:24:14 -0400

    add text editor

--- php_oop/photo_gallery/admin/includes/footer.php
+++ php_oop/photo_gallery/admin/includes/footer.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- WYSIWYG -->
+    <script src="https://cdn.tiny.cloud/1/mqsw64c7klnpl2glvjpwcasbl489oz7pyjmdyasd1tb8bc7f/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="./js/scripts.js"></script>
+
 </body>
 
 </html>
