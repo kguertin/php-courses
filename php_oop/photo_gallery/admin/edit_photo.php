83b3975a401f875aa488b4ff8becbb61a9148d10
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-16 20:26:44 -0400

    finish edit page

--- php_oop/photo_gallery/admin/edit_photo.php
+++ php_oop/photo_gallery/admin/edit_photo.php
@@ -1,6 +1,10 @@
 <?php include("includes/header.php"); ?>
 <?php if(!$session->is_signed_in()) {redirect('login.php'); } ?>
 <?php
+
+if(isset($_POST['update'])){
+    echo 'it works';
+}
     $photos = Photo::find_all();
 ?>
 
@@ -23,27 +27,61 @@
                     Photos
                     <small>Subheading</small>
                 </h1>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <input type="text" name="title" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="caption">Caption</label>
-                        <input type="text" name="caption" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="alt-text">Alternate Text</label>
-                        <input type="text" name="alt-text" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea class='form-control' name="description" id="" cols="30" rows="10"></textarea>
-                    </div>
-
-
+                <form action="edit_photo.php" method="POST">
+                <div class="col-md-8">
+                        <div class="form-group">
+                            <input type="text" name="title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="caption">Caption</label>
+                            <input type="text" name="caption" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="alt-text">Alternate Text</label>
+                            <input type="text" name="alt-text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea class='form-control' name="description" id="" cols="30" rows="10"></textarea>
+                        </div>
                 </div>
-         
-            </div>
+            
+                <div class="col-md-4" >
+                            <div  class="photo-info-box">
+                                <div class="info-box-header">
+                                   <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                                </div>
+                            <div class="inside">
+                              <div class="box-inner">
+                                 <p class="text">
+                                   <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                                  </p>
+                                  <p class="text ">
+                                    Photo Id: <span class="data photo_id_box">34</span>
+                                  </p>
+                                  <p class="text">
+                                    Filename: <span class="data">image.jpg</span>
+                                  </p>
+                                 <p class="text">
+                                  File Type: <span class="data">JPG</span>
+                                 </p>
+                                 <p class="text">
+                                   File Size: <span class="data">3245345</span>
+                                 </p>
+                              </div>
+                              <div class="info-box-footer clearfix">
+                                <div class="info-box-delete pull-left">
+                                    <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>   
+                                </div>
+                                <div class="info-box-update pull-right ">
+                                    <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                                </div>   
+                              </div>
+                            </div>          
+                        </div>
+                    </div>
+                    </form>
+                    </div>
         </div>
         <!-- /.row -->
 
