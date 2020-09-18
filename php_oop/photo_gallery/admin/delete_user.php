d904da7025a023fc1748c8f39ff905a255ee46bc
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-18 17:32:21 -0400

    make add user page

--- php_oop/photo_gallery/admin/delete_user.php
+++ php_oop/photo_gallery/admin/delete_user.php
@@ -3,15 +3,15 @@
 
 <?php 
 if(empty($_GET['id'])){
-    redirect('../users.php');
+    redirect('./users.php');
 }
 
 $user = User::find_by_id($_GET['id']);
 
 if($user) {
-    $user->delete_user();
-    redirect('../users.php');
+    $user->delete();
+    redirect('./users.php');
 } else {
-    redirect('../users.php');
+    redirect('./users.php');
 }
 
--- php_oop/photo_gallery/admin/users.php
+++ php_oop/photo_gallery/admin/users.php
@@ -19,10 +19,8 @@
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">
-                    users
-                    <small>Subheading</small>
-                </h1>
+                <h1 class="page-header">Users</h1>
+                <a class="btn btn-primary" href="add_user.php">Add User</a>
                 <div class="col-md-12">
                     <table class="table table-hover">
                         <thead>
@@ -43,7 +41,7 @@
                                         <td>
                                             <?php echo  $user->username; ?>
                                             <div class="action_links">
-                                                <a href="delete_user.php?id=<?php echo $user->id?>">Delete</a>
+                                                <a href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
                                                 <a href="edit_user.php?id=<?php echo $user->id ?>">Edit</a>
                                                 <a href="#">View</a>
                                             </div>
