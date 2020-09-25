71da10a5bb77e965c9c400156363869f1e0665aa
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-24 23:13:21 -0400

    create paginate class

---
+++ php_oop/photo_gallery/admin/includes/paginate.php
@@ -0,0 +1,11 @@
+<?php
+
+class Paginate {
+    public $page;
+    public $items_per_page;
+    public $total_items;
+
+    public function __construct($page = 1, $items_per_page = 4, $total_items = 0){
+        
+    }
+}
