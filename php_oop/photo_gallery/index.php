29c1caa1f92528842952e49f94a3b483c0d337c8
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-24 23:06:31 -0400

    create pagination variables for index

--- php_oop/photo_gallery/index.php
+++ php_oop/photo_gallery/index.php
@@ -1,5 +1,12 @@
 <?php include("includes/header.php"); ?>
-<?php $photos = Photo::find_all(); ?>
+<?php 
+    $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+    $items_per_page = 4;
+    $total_items = Photo::count_all();
+
+    $photos = Photo::find_all(); 
+
+?>
 
 
         <div class="row">
