2455348d2870370c5f75e33a07f610983a507549
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-27 14:02:31 -0400

    paginate constructor assigns attributes on creation

--- php_oop/photo_gallery/admin/includes/paginate.php
+++ php_oop/photo_gallery/admin/includes/paginate.php
@@ -1,11 +1,13 @@
 <?php
 
 class Paginate {
-    public $page;
+    public $current_page;
     public $items_per_page;
     public $total_items;
 
     public function __construct($page = 1, $items_per_page = 4, $total_items = 0){
-        
+        $this->current_page = (int)$page;
+        $this->items_per_page = (int)$items_per_page;
+        $this->total_items = (int)$total_items;
     }
 }
