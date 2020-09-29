38c8c9bc85b7618f2274fbb2b745cbe15ebacc79
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-09-29 12:24:04 -0400

    clean up code in admin

--- php_oop/photo_gallery/admin/includes/admin_content.php
+++ php_oop/photo_gallery/admin/includes/admin_content.php
@@ -44,7 +44,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#">
+                            <a href="photos.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">Total Photos in Gallery</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -68,7 +68,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#">
+                            <a href="users.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">Total Users</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -91,7 +91,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#">
+                            <a href="comments.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">Total Comments</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
